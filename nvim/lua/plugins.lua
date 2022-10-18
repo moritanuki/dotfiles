e79d8f2388e728b03678e514ea938a67ffe2a483
moritanuki <ilovfam12@gmail.com>
2022-10-19 08:29:02 +0900

    気になるプラグイン追加

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,9 +1,9 @@
 -- packer自動clone
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -22,18 +22,48 @@ vim.cmd([[
 -----------------
 -- plugins
 -----------------
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- parcker
-  use { 'wbthomason/packer.nvim' }
+  use "wbthomason/packer.nvim"
   -- status line
   use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons' }
+	  "nvim-lualine/lualine.nvim",
+	  requires = { "kyazdani42/nvim-web-devicons" }
 	}
+  -- buffer line
+  use {
+    "akinsho/bufferline.nvim", tag = "v3.*",
+    requires = "kyazdani42/nvim-web-devicons"
+  }
   -- file tree
-  use { "lambdalisue/fern.vim" }
+  use "lambdalisue/fern.vim"
+  use "lambdalisue/fern-git-status.vim"
   -- color scheme
   use { "EdenEast/nightfox.nvim", run = ":NightfoxCompile" }
   -- language server
-  use { 'neoclide/coc.nvim', branch = 'release' }
+  use { "neoclide/coc.nvim", branch = "release" }
+  -- comment out
+  use "terrortylor/nvim-comment"
+  -- todo comments
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {}
+    end
+  }
+  -- mode colors
+  use({
+    "mvllow/modes.nvim",
+    tag = "v0.2.0",
+    config = function()
+      require("modes").setup()
+    end
+  })
+  -- fuzzy finder
+  use {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    requires = { "kyazdani42/nvim-web-devicons" }
+  }
 end)
