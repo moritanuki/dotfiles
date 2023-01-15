c812e2895e8af2a15fbac0e915adb535dfe0e8dd
moritanuki <ilovfam12@gmail.com>
2023-01-15 09:04:35 +0900

    keymapにalias追加

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,4 +1,21 @@
--- vim.keymap.set(mode, lhs, rhs, options)
+-- alias
+g   = vim.g
+o   = vim.o
+opt = vim.opt
+cmd = vim.cmd
+fn  = vim.fn
+api = vim.api
+key = vim.keymap
+
+-- keymap
+g.mapleader = " "
 
 -- fern
-vim.keymap.set('n', '<Space>f', ':Fern .')
+key.set('n', '<C-e>', ':Fern . -reveal=% -drawer -toggle -width=33<CR>')
+
+-- telescope
+key.set('n', '<C-p>', '<cmd>Telescope find_files<CR>')
+key.set('n', '<C-g>', '<cmd>Telescope live_grep<CR>')
+
+-- lazyGit
+key.set('n', 'gl', ':LazyGit<CR>')
