798e587e7d0883d46aed84d233f62fcce779016e
moritanuki <ilovfam12@gmail.com>
2023-01-15 09:02:45 +0900

    lazyGit・teleScope導入

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -25,28 +25,32 @@ vim.cmd([[
 return require("packer").startup(function(use)
   -- parcker
   use "wbthomason/packer.nvim"
+  -- file tree
+  use "lambdalisue/fern.vim"
+  use "lambdalisue/fern-git-status.vim"
+  -- comment out
+  use "terrortylor/nvim-comment"
+  -- git
+  use 'kdheepak/lazygit.nvim'
+  -- language server
+  use { "neoclide/coc.nvim", branch = "release" }
+  -- git diff
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
   -- status line
   use {
-	  "nvim-lualine/lualine.nvim",
-	  requires = { "kyazdani42/nvim-web-devicons" }
-	}
+   "nvim-lualine/lualine.nvim",
+   requires = { "kyazdani42/nvim-web-devicons" }
+  }
   -- buffer line
   use {
     "akinsho/bufferline.nvim", tag = "v3.*",
     requires = "kyazdani42/nvim-web-devicons"
   }
-  -- file tree
-  use "lambdalisue/fern.vim"
-  use "lambdalisue/fern-git-status.vim"
   -- color scheme
   use {
     "phha/zenburn.nvim",
     config = function() require("zenburn").setup() end
   }
-  -- language server
-  use { "neoclide/coc.nvim", branch = "release" }
-  -- comment out
-  use "terrortylor/nvim-comment"
   -- todo comments
   use {
     "folke/todo-comments.nvim",
@@ -63,30 +67,21 @@ return require("packer").startup(function(use)
       require("modes").setup()
     end
   })
-  -- fuzzy finder
-  use { "junegunn/fzf", run = ":call fzf#install()" }
-  use {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    requires = { "kyazdani42/nvim-web-devicons" }
-  }
-  -- git diff
-  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
   -- noice
   use({
-  "folke/noice.nvim",
-  config = function()
-    require("noice").setup({
-        -- add any options here
-    })
-  end,
-  requires = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup({
+      })
+    end,
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
     }
-})
+  })
+  -- telescope
+  use {
+   'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
